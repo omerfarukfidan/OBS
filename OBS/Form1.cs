a8d55df12ecc55517a3c52de84d73eb5fb2f4583
Ömer Faruk Fidan <fidanomerfaruk01@gmail.com>
2023-08-17 14:30:14 +0300

    test

--- OBS/Form1.cs
+++ OBS/Form1.cs
@@ -13,11 +13,13 @@ namespace OBS
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        
         public string conString = "Data Source=DESKTOP-31EMK53\\SQLEXPRESS;Initial Catalog=OBS;Persist Security Info=True;User ID=obsUser;Password=1234";
 
 
